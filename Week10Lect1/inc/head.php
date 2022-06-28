bcedc9bb00f3843684753c4ee6e7f4e9b9c80a33
spenxeram <55945082+spenxeram@users.noreply.github.com>
2022-06-28 09:03:13 +0700

    Week10Lect1
    Start files for Github collab activity

---
+++ Week10Lect1/inc/head.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;700&family=Montserrat:wght@100;400;700&family=Poppins:wght@300;700&family=Raleway:wght@500;900&family=Roboto+Slab:wght@100;600;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Crypto.VN</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-md">
+      <div class="container">
+        <a href="index.php" class="navbar-brand">Crypto<span class="text-info">.VN</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog.php">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact.php</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
