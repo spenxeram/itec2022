584ee5956b67635c47d25c21ca97c56f10846653
spenxeram <55945082+spenxeram@users.noreply.github.com>
2022-06-28 09:26:03 +0700

    about us

---
+++ Week10Lect1/about-us.php
@@ -0,0 +1,14 @@
+<?php 
+    include "inc/head.php";
+?>
+
+<div class="jumbotron">
+    <h1 class="display-4">About US</h1>
+    <p class="lead">Committed to Github</p>
+    <hr class="my-4">
+    <p>Content</p>
+</div>
+
+<?php
+    include "inc/footer.php";
+?>
