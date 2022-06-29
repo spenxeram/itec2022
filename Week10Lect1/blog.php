f9b433f79956159cc798194398630c09f1b583ef
spenxeram <55945082+spenxeram@users.noreply.github.com>
2022-06-29 13:21:53 +0700

    Week10Lect2

---
+++ Week10Lect1/blog.php
@@ -0,0 +1,14 @@
+<?php
+include "inc/head.php";
+?>
+
+<div class="jumbotron">
+    <h1 class="display-4">Blog</h1>
+    <p class="lead">Subtitle</p>
+    <hr class="my-4">
+    <p>Content</p>
+</div>
+
+<?php
+    include "inc/footer.php"
+?>
---
+++ Week10Lect2/index.php
@@ -0,0 +1,100 @@
+<?php
+include "inc/head.php";
+?>
+    <div class="jumbotron hero text-center text-white">
+      <div class="container py-4">
+        <h1 class="display-5">#1 Crypto Exchange in Vietnam</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button type="button" class="btn btn-info btn-lg px-5" name="button">Read More</button>
+      </div>
+    </div>
+    <main>
+      <div class="container">
+        <h2>TIN HOAT DONG</h2>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card">
+              <img class="card-img-top" src="images/front1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Go to Exchange</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-info btn-block">Go somewhere</a>
+              </div>
+            </div> <!-- end of .card -->
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="card">
+              <img class="card-img-top" src="images/front2.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Decentralized Finance</h5>
+                <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <a href="#" class="btn btn-info btn-block">Go somewhere</a>
+              </div>
+            </div> <!-- end of .card -->
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="card">
+              <img class="card-img-top" src="images/front3.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Privacy</h5>
+                <p class="card-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" class="btn btn-info btn-block">Go somewhere</a>
+              </div>
+            </div> <!-- end of .card -->
+          </div>
+        </div>
+      </div>
+
+      <div class="jumbotron-fluid banner2 py-5 my-5">
+        <div class="py-5">
+
+        </div>
+      </div>
+      <div class="container my-5">
+        <div class="row">
+          <div class="col-md-6">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.4623247798404!2d106.68145350503737!3d10.761995501101053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1c06f4e1dd%3A0x43900f1d4539a3d!2sUniversity%20of%20Science%20-%20VNUHCM!5e0!3m2!1sen!2s!4v1644896237208!5m2!1sen!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div>
+          <div class="col-md-6">
+            <h2>Contact<span class='text-info'>Us</span></h2>
+            <form class="" action="index.html" method="post">
+              <div class="form-group">
+                <label for="name">Your Name</label>
+                <input type="text" name="name" class="form-control" placeholder="Enter your name...">
+              </div>
+              <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" class="form-control" name="email" placeholder="Enter your email...">
+              </div>
+              <div class="form-group">
+                <label for="message">Your Message</label>
+                <textarea name="message" rows="4" class="form-control"></textarea>
+              </div>
+              <div class="form-check">
+                <input type="checkbox" name="subscribe" value="true" class="form-check-input">
+                <label for="subscribe" class="text-muted form-check-label">Subscribe to our newsletter</label>
+              </div>
+              <button type="submit" name="button" class="btn btn-info btn-block mt-3">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="mt-5 py-5 bg-dark text-white">
+      <div class="container pb-5">
+        <div class="d-flex justify-content-between">
+          <h3>Crypto<span class="text-info">.VN</span> 2022</h3>
+          <div class="d-flex">
+            <img src="images/facebook.png" width="45px" height="45px" class="pl-1" alt="">
+            <img src="images/youtube.png" width="45px" height="45px" class="pl-1" alt="">
+            <img src="images/twitter.png" width="45px" height="45px" class="pl-1" alt="">
+            <img src="images/rss.png" width="45px" height="45px" class="pl-1" alt="">
+            <img src="images/share.png" width="45px" height="45px" class="pl-1" alt="">
+          </div>
+        </div>
+
+      </div>
+
+<?php
+  include "inc/footer.php";
+?>
