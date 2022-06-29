93ecfa384e7eaadcd106079f875fc363595186fc
spenxeram <55945082+spenxeram@users.noreply.github.com>
2022-06-29 13:28:04 +0700

    about us

--- Week10Lect2/about-us.php
+++ Week10Lect2/about-us.php
@@ -1,1 +1,13 @@
-trait_exists
+<?php
+include "inc/head.php";
+?>
+<div class="jumbotron">
+    <h1 class="display-4">About Us</h1>
+    <p class="lead">Push to Git</p>
+    <hr class="my-4">
+    <p>Content</p>
+</div>
+
+<?php
+include "inc/footer.php";
+?>
